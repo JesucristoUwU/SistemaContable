80063a04506939eb7480ed533aa6c9f5f16c95de
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-04 21:38:13 -0600

    Conf

--- Recursos/Conexion/ConexionAccess.java
+++ Recursos/Conexion/ConexionAccess.java
@@ -1,6 +1,7 @@
 package Recursos.Conexion;
 import java.sql.*;
 import java.util.*;
+import javax.swing.*;
 public class ConexionAccess {
     Connection c=null;
     Statement st=null;
@@ -8,9 +9,14 @@ public class ConexionAccess {
 
     public ConexionAccess(){
         try {
-            
-        } catch (Exception e) {
+            String rutaDb="C:\\Users\\steph\\OneDrive\\Documentos\\GitHub\\SistemaContable\\BaseDeDatos.accdb";
+            String url="";
+            c=DriverManager.getConnection(url);
+            st=c.createStatement();
 
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al Conectar con la Base de Datos"+e);
+            System.exit(0);
         }
     }
 }
