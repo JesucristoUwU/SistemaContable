7ec4cac1ea5255f2792c97baf799da612a488239
StephTeno <stephanietenoriocarolinaorozco@gmail.com>
2022-06-04 21:10:40 -0600

    Conf

--- Recursos/Conexion/ConexionAccess.java
+++ Recursos/Conexion/ConexionAccess.java
@@ -2,5 +2,15 @@ package Recursos.Conexion;
 import java.sql.*;
 import java.util.*;
 public class ConexionAccess {
-    
+    Connection c=null;
+    Statement st=null;
+    ResultSet rs=null;
+
+    public ConexionAccess(){
+        try {
+            
+        } catch (Exception e) {
+
+        }
+    }
 }
